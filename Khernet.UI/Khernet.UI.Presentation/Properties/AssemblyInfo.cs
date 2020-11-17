34a0c7cd457a400002274323efdf464a45debd22
Luis <info@lemaluis.com>
2020-11-16 23:00:53 -0500

    Changed style of scrollbars.

--- Khernet.UI/Khernet.UI.Presentation/Properties/AssemblyInfo.cs
+++ Khernet.UI/Khernet.UI.Presentation/Properties/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Windows;
 // Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión 
 // mediante el carácter '*', como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.13.10.0")]
-[assembly: AssemblyFileVersion("0.13.10.0")]
+[assembly: AssemblyVersion("0.13.11.0")]
+[assembly: AssemblyFileVersion("0.13.11.0")]
