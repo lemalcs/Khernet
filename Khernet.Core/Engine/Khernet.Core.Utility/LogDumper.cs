e4089a3aff09b5be6a137f94f424107e90bceb07
Luis <info@lemaluis.com>
2021-04-03 18:45:04 -0500

    Modified format of log file for Khernet.exe assembly.

--- Khernet.Core/Engine/Khernet.Core.Utility/LogDumper.cs
+++ Khernet.Core/Engine/Khernet.Core.Utility/LogDumper.cs
@@ -9,17 +9,12 @@ namespace Khernet.Core.Utility
 {
     public static class LogDumper
     {
-#if DEBUG
         private static Logger logger = LogManager.GetCurrentClassLogger();
-#endif
         static LogDumper()
         {
-#if DEBUG
             LoggingConfiguration config = new LoggingConfiguration();
             FileTarget target = new FileTarget("KhernetLog");
-            target.Layout = Layout.FromString(
-                "${longdate}|${level:uppercase=true}|${logger}|${identity}|${processinfo}${newline}${message}${newline}${stacktrace}${newline}${exception}${newline}"
-                );
+            target.Layout = Layout.FromString("${longdate} > | ${level:uppercase=true} | ${message}");
 
             var assembly = System.Reflection.Assembly.GetEntryAssembly();
             target.FileName = Path.Combine(Path.GetDirectoryName(assembly.Location), "Khernet_LOG.txt");
@@ -27,34 +22,81 @@ namespace Khernet.Core.Utility
             config.AddRuleForAllLevels(target);
 
             LogManager.Configuration = config;
-
-#endif
         }
 
 
-        public static void WriteLog(Exception exception, string sourceMethod)
+        public static void WriteLog(Exception exception, string message)
         {
-#if DEBUG
-            logger.Error(exception, sourceMethod);
-            if (exception != null && exception.InnerException != null)
-                logger.Error(exception.InnerException, sourceMethod);
-#endif
+            string formatted_message = string.Concat(
+                "---------------------------------------------------------------------------------------------",
+                "\r\n",
+                message,
+                "\r\n",
+                "---------------------------------------------------------------------------------------------",
+                "\r\n");
+            WriteInformationLines(formatted_message);
+
+            if (exception == null)
+                return;
+
+            WriteExceptionLines(exception);
+
+            if (exception.InnerException != null)
+                WriteExceptionLines(exception.InnerException);
         }
 
         public static void WriteLog(Exception exception)
         {
-#if DEBUG
-            logger.Error(exception);
-            if (exception != null && exception.InnerException != null)
-                logger.Error(exception.InnerException);
-#endif
+            if (exception == null)
+                return;
+
+            WriteExceptionLines(exception);
+
+            if (exception.InnerException != null)
+                WriteExceptionLines(exception.InnerException);
         }
         public static void WriteInformation(string text)
         {
-#if DEBUG
-            logger.Log(LogLevel.Info, text);
-#endif
+            WriteInformationLines(text);
+        }
 
+        /// <summary>
+        /// Writes en entry to log with details of <see cref="Exception "/>.
+        /// </summary>
+        /// <param name="exception">The exception that needs to be written to log.</param>
+        private static void WriteExceptionLines(Exception exception)
+        {
+            if (exception == null)
+                return;
+
+            string[] lines = exception.Message.Split('\n');
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                logger.Log(LogLevel.Error, lines[i]);
+            }
+
+            lines = exception.StackTrace.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < lines.Length; i++)
+            {
+                logger.Log(LogLevel.Error, lines[i]);
+            }
+        }
+
+        /// <summary>
+        /// Writes a message to log.
+        /// </summary>
+        /// <param name="text">The message to write to.</param>
+        private static void WriteInformationLines(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return;
+
+            string[]lines = text.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < lines.Length; i++)
+            {
+                logger.Log(LogLevel.Info, lines[i]);
+            }
         }
     }
 }
