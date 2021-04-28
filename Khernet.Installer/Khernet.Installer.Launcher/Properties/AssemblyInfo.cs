a371a605a5646283316156bf6245127d778b016c
Luis <info@lemaluis.com>
2021-04-27 23:14:05 -0500

    Links added to About page.

--- Khernet.Installer/Khernet.Installer.Launcher/Properties/AssemblyInfo.cs
+++ Khernet.Installer/Khernet.Installer.Launcher/Properties/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Windows;
 // Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
 // utilizando el carácter "*", como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.15.5")]
-[assembly: AssemblyFileVersion("0.15.5")]
+[assembly: AssemblyVersion("0.15.6")]
+[assembly: AssemblyFileVersion("0.15.6")]
--- Khernet.UI/Khernet.UI.Presentation/ViewModels/Dialogs/AboutViewModel.cs
+++ Khernet.UI/Khernet.UI.Presentation/ViewModels/Dialogs/AboutViewModel.cs
@@ -1,10 +1,13 @@
-﻿using System.Diagnostics;
+﻿using Khernet.Core.Utility;
+using Khernet.UI.Cache;
+using System;
+using System.Diagnostics;
 using System.Windows.Input;
 
 namespace Khernet.UI
 {
     /// <summary>
-    /// View model for user profile.
+    /// View model for about information.
     /// </summary>
     public class AboutViewModel : BaseModel
     {
@@ -12,20 +15,43 @@ namespace Khernet.UI
         #region Commands
 
         /// <summary>
-        /// Clear the files located in cache directory.
+        /// Opens the issue page on browser.
         /// </summary>
         public ICommand OpenIssueCommand { get; private set; }
+
+        /// <summary>
+        /// Opens the source code page on browser.
+        /// </summary>
+        public ICommand OpenSourceCodeCommand { get; private set; }
+
+        /// <summary>
+        /// Opens the directory where this application is installed. 
+        /// </summary>
+        public ICommand OpenInstallationPathCommand { get; private set; }
 
         #endregion
 
         public AboutViewModel()
         {
             OpenIssueCommand = new RelayCommand(OpenIssue);
+            OpenSourceCodeCommand = new RelayCommand(OpenSourceCode);
+            OpenInstallationPathCommand = new RelayCommand(OpenInstallationPath);
         }
 
-        private void OpenIssue(object obj)
+        private void OpenSourceCode()
+        {
+            Process.Start("https://github.com/lemalcs/Khernet");
+        }
+
+        private void OpenIssue()
         {
             Process.Start("https://github.com/lemalcs/Khernet-issues/issues");
+        }
+
+        private void OpenInstallationPath()
+        {
+            Process.Start(Configurations.HomeDirectory);
+
         }
 
     }
