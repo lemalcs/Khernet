9b09e1dcaaee67096e601c0afac82da9377ee83b
Luis <info@lemaluis.com>
2021-06-12 19:26:13 -0500

    Khernet.Installer solution created.

---
+++ Khernet.Installer/Khernet.Installer/Khernet.Installer.Launcher/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Input;
+
+namespace Khernet.Installer.Launcher
+{
+    /// <summary>
+    /// Main windows for installation.
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            DragMove();
+        }
+    }
+}
---
+++ Khernet.Installer/Khernet.Installer/Khernet.Installer.Launcher/ViewModels/BaseModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace Khernet.Installer.Launcher
+{
+    public class BaseModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Fire an event when property changes.
+        /// </summary>
+        /// <param name="property">Property name.</param>
+        protected virtual void OnPropertyChanged(string property)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
