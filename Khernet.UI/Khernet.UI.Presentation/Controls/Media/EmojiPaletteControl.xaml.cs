2a9f76543ef5bf04a4b40a60b13da43ba0a943d4
Luis <info@lemaluis.com>
2022-01-15 14:30:51 -0500

    Scroll to home when emoji openned and fix issue that showed a blank list when palette was openned.

--- Khernet.UI/Khernet.UI.Presentation/Controls/Media/EmojiPaletteControl.xaml.cs
+++ Khernet.UI/Khernet.UI.Presentation/Controls/Media/EmojiPaletteControl.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace Khernet.UI.Controls
 {
@@ -12,6 +13,11 @@ namespace Khernet.UI.Controls
         /// Fired when a emoji is selected from list.
         /// </summary>
         public event EventHandler<SelectedEmojiEventArgs> SelectedEmoji;
+
+        /// <summary>
+        /// The <see cref="ScrollViewer"/> owned by <see cref="ListBox"/> container.
+        /// </summary>
+        private ScrollViewer scrollViewer;
 
         public EmojiPaletteControl()
         {
@@ -34,6 +40,49 @@ namespace Khernet.UI.Controls
         protected void OnSelectedEmoji(string emojiCode)
         {
             SelectedEmoji?.Invoke(this, new SelectedEmojiEventArgs(emojiCode));
+        }
+
+        private void emojiControl_IsVisibleChanged(object sender, System.Windows.DependencyPropertyChangedEventArgs e)
+        {
+            if(!emojiControl.IsLoaded)
+                return;
+
+            if (scrollViewer == null)
+                scrollViewer = FindVisualChild<ScrollViewer>(emojiControl);
+
+            // Scroll to the top of the emojis list whenever the palette is showed
+            if (scrollViewer != null)
+                scrollViewer.ScrollToHome();
+        }
+
+        /// <summary>
+        /// Search for an element of a certain type in the visual tree.
+        /// </summary>
+        /// <typeparam name="T">The type of element to find.</typeparam>
+        /// <param name="visual">The parent element.</param>
+        /// <returns></returns>
+        private T FindVisualChild<T>(Visual visual) where T : Visual
+        {
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(visual); i++)
+            {
+                Visual child = (Visual)VisualTreeHelper.GetChild(visual, i);
+                if (child != null)
+                {
+                    T correctlyTyped = child as T;
+                    if (correctlyTyped != null)
+                    {
+                        return correctlyTyped;
+                    }
+
+                    T descendent = FindVisualChild<T>(child);
+                    if (descendent != null)
+                    {
+                        return descendent;
+                    }
+                }
+            }
+
+            return null;
         }
     }
 }
